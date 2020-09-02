821cf557394a0f24a1ab6abf0ea11f9a5b6bc597
Kiddos <em.lunaolivz@gmail.com>
2020-09-01 21:14:52 -0600

    This my Average CSharp Program

---
+++ Average CSharp Program/Average CSharp Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppEmmanuelAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double score1, score2, score3;
+            int averageScore;
+
+            score1 = 87;
+            score2 = 83;
+            score3 = 78;
+
+            averageScore = (int)((score1 + score2 + score3) / 3);
+
+            Console.WriteLine("Scores");
+            Console.WriteLine("exam 1= {0}", score1);
+            Console.WriteLine("exam 2= {0}", score2);
+            Console.WriteLine("exam 3= {0}", score3);
+            Console.WriteLine("average----> {0}", averageScore);
+
+            Console.ReadKey();
+        }
+    }
+}
